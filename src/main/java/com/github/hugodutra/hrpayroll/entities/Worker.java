c977b6815ad32f333ede9b307cdcf9038466595b
hugo-dutra <hugo.dutra@hotmail.com>
2022-02-02 19:13:05 -0300

    Teste com restTemplate

---
+++ src/main/java/com/github/hugodutra/hrpayroll/entities/Worker.java
@@ -0,0 +1,16 @@
+package com.github.hugodutra.hrpayroll.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Worker implements Serializable {
+    private Long id;
+    private String name;
+    private Double daylyIncome;
+}
--- src/main/java/com/github/hugodutra/hrpayroll/services/PaymentService.java
+++ src/main/java/com/github/hugodutra/hrpayroll/services/PaymentService.java
@@ -1,13 +1,29 @@
 package com.github.hugodutra.hrpayroll.services;
 
 import com.github.hugodutra.hrpayroll.entities.Payment;
+import com.github.hugodutra.hrpayroll.entities.Worker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PaymentService {
 
-    public Payment getPayment(Long workerId, int days){
-        return new Payment("Bob",200.0,days);
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Value("${hr-worker.host}")
+    private String WORKER_HOST;
+
+    public Payment getPayment(Long workerId, int days) {
+        Map<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("id", workerId.toString());
+        Worker worker = restTemplate.getForObject(WORKER_HOST + "/api/workers/{id}", Worker.class, uriVariables);
+        return new Payment(worker.getName(), worker.getDaylyIncome(), days);
     }
 
 }
