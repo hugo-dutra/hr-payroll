947f18dcf2733cd611c6e6885a4aeba14c49a457
hugo-dutra <hugo.dutra@hotmail.com>
2022-02-03 09:35:45 -0300

    Balanceamento de carga com ribbon

--- src/main/java/com/github/hugodutra/hrpayroll/feignclients/WorkerFeignClient.java
+++ src/main/java/com/github/hugodutra/hrpayroll/feignclients/WorkerFeignClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @Component
-@FeignClient(name="hr-worker", url = "http://localhost:8001",path = "/api/workers")
+@FeignClient(name="hr-worker", path = "/api/workers")
 public interface WorkerFeignClient {
     @GetMapping("/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id);
