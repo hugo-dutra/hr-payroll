2ef6e6f9b3e9da52730da711ad4165a4a8bbc8c5
hugo-dutra <hugo.dutra@hotmail.com>
2022-02-03 19:21:06 -0300

    added hystrix project

--- src/main/java/com/github/hugodutra/hrpayroll/resources/PaymentResource.java
+++ src/main/java/com/github/hugodutra/hrpayroll/resources/PaymentResource.java
@@ -2,6 +2,7 @@ package com.github.hugodutra.hrpayroll.resources;
 
 import com.github.hugodutra.hrpayroll.entities.Payment;
 import com.github.hugodutra.hrpayroll.services.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,9 +16,15 @@ public class PaymentResource {
     @Autowired
     private PaymentService paymentService;
 
+    @HystrixCommand(fallbackMethod = "getPaymentAlternative")
     @GetMapping("/{workerId}/days/{days}")
-    private ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days){
+    public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days){
         Payment payment = paymentService.getPayment(workerId,days);
+        return ResponseEntity.ok(payment);
+    }
+
+    public ResponseEntity<Payment> getPaymentAlternative(Long workerId, Integer days){
+        Payment payment = new Payment("Brann",400.0,days);
         return ResponseEntity.ok(payment);
     }
 }
